06c0c8879dd2c32c42ecfb085d7e3920551a0be6
Ian Stewart <scrael@protonmail.ch>
2025-12-30 17:34:42 -0800

    Message builder and sharding examples

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,33 @@ use serenity::builder::CreateMessage;
 use serenity::model::channel::Message;
 use serenity::model::gateway::Ready;
 use serenity::prelude::*;
+use serenity::utils::MessageBuilder;
 
 struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
     async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "!ping"
-        && let Err(e) = msg.channel_id.say(&ctx.http, "Pong!").await {
-            println!("Error sending message: {e:?}");
+        if msg.content == "!ping" {
+            let channel = match msg.channel_id.to_channel(&ctx).await {
+                Ok(channel) => channel,
+                Err(e) => {
+                    println!("Error getting channel: {e:?}");
+                    return;
+                }
+            };
+
+            let response = MessageBuilder::new()
+                .push("User ")
+                .push_bold_safe(&msg.author.name)
+                .push(" used the 'ping' command in the ")
+                .mention(&channel)
+                .push(" channel")
+                .build();
+
+            if let Err(e) = msg.channel_id.say(&ctx.http, &response).await {
+                println!("Error sending message: {}", e);
+            }
         }
 
         if msg.content == "!messageme" {
@@ -29,7 +47,9 @@ impl EventHandler for Handler {
     }
 
     async fn ready(&self, _: Context, ready: Ready) {
-        println!("{} is connected!", ready.user.name);
+        if let Some(shard) = ready.shard {
+            println!("{} is connected on shard {}/{}!", ready.user.name, shard.id, shard.total);
+        }
     }
 }
 
@@ -46,7 +66,7 @@ async fn main() {
     let mut client =
         Client::builder(&token, intents).event_handler(Handler).await.expect("Err creating client");
 
-    if let Err(e) = client.start().await {
+    if let Err(e) = client.start_shards(2).await {
         println!("Client error: {e:?}");
     }
 }
