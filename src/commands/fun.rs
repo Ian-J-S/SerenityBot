3fa3ec273bad2a84f70f1796f452a514ad14222f
Ian Stewart <scrael@protonmail.ch>
2026-01-14 17:31:14 -0800

    Fix get_last_message offset

--- src/commands/fun.rs
+++ src/commands/fun.rs
@@ -284,9 +284,17 @@ fn choose_ban_msg(user_mentioned: Mention) -> String {
 async fn get_last_message(ctx: &Context<'_>) -> Result<serenity::Message, Error> {
     let channel = ctx.channel_id();
 
+    // If prefix command is used, it counts as the last message, so we need 2 messages.
+    // Otherwise, just 1 message is needed.
+    let limit = if let poise::Context::Prefix(_) = ctx {
+        2
+    } else {
+        1
+    };
+
     let messages = channel
         // Limit to 2 (not 1) or else the ban command itself is chosen
-        .messages(ctx, GetMessages::new().limit(2))
+        .messages(ctx, GetMessages::new().limit(limit))
         .await?;
 
     let message = messages.last()
